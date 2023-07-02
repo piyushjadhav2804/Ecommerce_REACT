a1868d51d977a79a872d999e870c5ef342772bb0
Piyush-Jadhav2804 <71430828+Piyush-Jadhav2804@users.noreply.github.com>
2023-07-03 00:47:22 +0530

    done with the project

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { legacy_createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
@@ -13,6 +13,6 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const store = legacy_createStore(persistedReducer, applyMiddleware(thunk));
 
 export const persistor = persistStore(store);
